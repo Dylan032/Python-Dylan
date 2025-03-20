0f11a1f33c4b9e180b154547540b027db4f951b2
Dylan032 <97119365@st.deltion.nl>
2025-03-20 11:51:59 +0100

    Add files via upload

---
+++ oh een tuple dylan.py
@@ -0,0 +1,11 @@
+# Opdracht 1
+# Naam student:
+# Groep:
+
+# Hier komt je code...
+
+tuple = (11, "Oh een tuple", 13.5, 8009)
+print(tuple)    
+for item in tuple:
+    print(item)
+# output: (11, 'Oh een tuple', 13.5, 8009)
