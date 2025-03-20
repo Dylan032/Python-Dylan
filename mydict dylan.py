ed10678b7c61567423bf3cf7afe418010601e8b3
Dylan032 <97119365@st.deltion.nl>
2025-03-20 11:55:50 +0100

    Add files via upload

---
+++ mydict dylan.py
@@ -0,0 +1,15 @@
+# Opdracht 1
+# Naam student:
+# Groep:
+
+# Hier komt je code...
+
+my_dict = {
+	"naam": "willem van der broek",
+	"leeftijd": 23,
+	"favoriete kleur": "blauw"
+}
+print(my_dict)
+for item in my_dict:
+	print(item, my_dict[item])  
+	
