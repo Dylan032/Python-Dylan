38fbbffea1b3780fa376586f34a5b1bc583944fe
Dylan032 <97119365@st.deltion.nl>
2025-03-20 11:49:25 +0100

    Add files via upload

---
+++ wat een weertje dylan.py
@@ -0,0 +1,12 @@
+# Opdracht 1
+# Naam student:Dylan vermeer
+# Groep:
+
+# Hier komt je code...
+my_list = [13, "wat een weertje", 12.5, 8]
+print(my_list)
+for item in my_list:
+    print(item)
+# output: [13, 'wat een weertje', 12.5, 8]
+# output: laat alle tekst onder elkaar zien in een lijst
+
