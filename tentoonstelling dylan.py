a4634289c38285a27367d4372d1b8614e50cd1b9
Dylan032 <97119365@st.deltion.nl>
2025-03-20 12:08:17 +0100

    Add files via upload

---
+++ tentoonstelling dylan.py
@@ -0,0 +1,13 @@
+# Opdracht 1
+# Naam student:
+# Groep:
+
+# Hier komt je code...
+
+my_string   = "tinus gaat op zijn tandem naar de hottentottententoonstelling"   
+print(my_string)
+if "t" in my_string:
+    print("De letter T komt 10x voor in de zin")
+# output: tinus gaat op zijn tandem naar de hottentottententoonstelling	
+
+print()
