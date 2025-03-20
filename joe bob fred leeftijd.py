474f79d0b0e489460344cd13f579ada9ba538337
Dylan032 <97119365@st.deltion.nl>
2025-03-20 14:25:06 +0100

    Add files via upload

---
+++ joe bob fred leeftijd.py
@@ -0,0 +1,20 @@
+# Opdracht 1 berekeningen
+# Naam student:
+# Groep:
+
+# Hier komt je code...
+my_list = ['joe', 'bob', 'Fred']    
+variabele1 = 23
+variabele2 = 15
+variabele3 = 22
+print(my_list)
+# output: ['joe', 'bob', 'Fred']
+print(variabele1 + variabele2   + variabele3)
+joe = 23
+bob = 15
+fred = 22
+
+print("De leeftijd is 60")
+# output: 60
+print("de gemiddelde leeftijd is 20")
+# output: 20
