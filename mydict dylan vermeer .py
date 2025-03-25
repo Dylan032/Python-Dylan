ee68f5823d6c0ffbe82feccbd99f70eaf914c3e5
Dylan032 <97119365@st.deltion.nl>
2025-03-25 11:45:37 +0100

    Add files via upload

---
+++ mydict dylan vermeer .py
@@ -0,0 +1,24 @@
+# Opdracht 1 lists
+# Naam student:
+# Groep:
+
+mylist1 = ["pieter jan", "klaas"]   
+print(mylist1)
+mylist2 = ["henk peter", "piet"] 
+print(mylist2)
+mylist3 = ["jantje henk", "klaas"]
+print(mylist3)
+mylist4= ["peter jan", "pan"]
+print(mylist4)
+result = mylist1 + mylist2 + mylist3 + mylist4
+print(result)
+# output: ['pieter jan', 'klaas']
+names_dict = {
+    "mylist1": mylist1,
+    "mylist2": mylist2,
+    "mylist3": mylist3,
+    "mylist4": mylist4
+}
+print(names_dict["mylist2"])
+
+
