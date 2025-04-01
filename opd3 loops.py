492abc55ca2493d7702fd925bade3cba05a9351a
Dylan032 <97119365@st.deltion.nl>
2025-04-01 11:40:48 +0200

    Add files via upload

---
+++ opd3 loops.py
@@ -0,0 +1,11 @@
+# Opdracht 3 input functie
+# Naam student:
+# Groep:
+
+# Hier komt je code...
+
+my_list = []
+for i in range(3, 82, 3):
+    result = (i ** 2) / 3
+    my_list.append(result)
+print(my_list)
