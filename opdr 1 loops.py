7af1ad236084cd2bf263ae898ca8a7d5bf9dd821
Dylan032 <97119365@st.deltion.nl>
2025-04-01 11:11:02 +0200

    Add files via upload

---
+++ opdr 1 loops.py
@@ -0,0 +1,22 @@
+# Opdracht 2 berekeningen
+# Naam student:
+# Groep:
+
+# Hier komt je code...
+
+
+c = -12
+f = 102
+
+print("-12 graden Celsius is gelijk aan 10.4 graden Fahrenheit")
+# output: -12 graden Celsius is gelijk aan 10.4 graden Fahrenheit
+print("102 graden Fahrenheit is gelijk aan 38.9 graden Celsius")
+# output: 102 graden Fahrenheit is gelijk aan 38.9 graden Celsius
+
+c =62.2
+f = 96
+
+print("62.2 graden Celsius is gelijk aan 144 graden Fahrenheit")
+# output: 62.2 graden Celsius is gelijk aan 144 graden Fahrenheit
+print("96 graden Fahrenheit is gelijk aan 35.6 graden Celsius")
+# output: 96 graden Fahrenheit is gelijk aan 35.6 graden Celsius
