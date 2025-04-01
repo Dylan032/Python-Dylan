30d47be01d85602bfd9ad010e07bcdf83580f32e
Dylan032 <97119365@st.deltion.nl>
2025-04-01 11:03:36 +0200

    Add files via upload

---
+++ opdr_1 input function.py
@@ -0,0 +1,32 @@
+# Opdracht 1 loops
+# Naam student:
+# Groep:
+
+# Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
+
+# Hier start de for-loop....
+
+# Opdracht 1 loops
+# Naam student:
+# Groep:
+
+# Hier komt je code, maak gebruik van de input functie om de lengte van de rechthoekzijden van de driehoek op te vragen.
+
+# Hier start de for-loop....
+
+# Vraag de lengte van de rechthoekzijden van de driehoek op
+triangle_sides = []
+
+for i in range(3):
+    side_length = float(input(f"Voer de lengte van rechthoekzijde {i + 1} in: "))
+    triangle_sides.append(side_length)
+
+print("Lengtes van de rechthoekzijden:", triangle_sides)
+
+# Maak een lijst met getallen van 1 tot en met 10
+number_list = []
+
+for i in range(1, 11):
+    number_list.append(i)
+
+print("Lijst met getallen van 1 tot en met 10:", number_list)1
